00673594abd51ddc75eb83253915966c8c051f97
Wong Kai Chong 黄恺聪 <KAI_Wong@outlook.com>
2025-06-12 09:20:17 +0800

    correct visual formatting and textual content.
    correct descriptions of jobs to be single-line. Added in previous work experience, and changed it from Coding Instructor to Workshop Facilitator as that's a more accurate term for my role.

--- src/components/WorkHistory.tsx
+++ src/components/WorkHistory.tsx
@@ -10,22 +10,16 @@ const WorkHistory = () => {
               <span>Responsibilities:</span>
               <ul>
                 <li>
-                  Engineered highly secure and scalable software features and
-                  fixes, driving tangible improvements to product performance.
+                  Engineered highly secure and scalable software features and fixes, driving tangible improvements to product performance.
                 </li>
                 <li>
-                  Implemented comprehensive testing strategies to ensure optimal
-                  product performance and identify critical areas for
-                  optimization.
+                  Implemented comprehensive testing strategies to ensure optimal product performance and identify critical areas for optimization.
                 </li>
                 <li>
-                  Developed and maintained clean, readable, and maintainable
-                  code, promoting long-term project sustainability.
+                  Developed and maintained clean, readable, and maintainable code, promoting long-term project sustainability.
                 </li>
                 <li>
-                  Worked within an agile team to resolve issues and deliver new
-                  features, contributing to the successful execution of product
-                  roadmaps.
+                  Worked within an agile team to resolve issues and deliver new features, contributing to the successful execution of product roadmaps.
                 </li>
               </ul>
               <span>Skills:</span>
@@ -42,8 +36,7 @@ const WorkHistory = () => {
             <ul>
               <li>Completed course for React.js on FreeCodeCamp.</li>
               <li>
-                Completed 14/14 available courses on Pluralsight's Technology
-                Quotient (TQ).
+                Completed 14/14 available courses on Pluralsight's Technology Quotient (TQ).
                 <br />
                 *topics passed in TQ are: Quantum Computing, XR and the
                 Metaverse, Security, Data, Sustainability & Technology, 5G &
@@ -60,17 +53,13 @@ const WorkHistory = () => {
             <p>01/2020 - 04/2020</p>
             <ul>
               <li>
-                Engineered sophisticated data filtering and feature enhancements
-                for an internal web tool, streamlining data access and analysis.
+                Engineered sophisticated data filtering and feature enhancementsfor an internal web tool, streamlining data access and analysis.
               </li>
               <li>
-                Executed critical programming modifications to an internal web
-                tool, improving system stability and operational efficiency.
+                Executed critical programming modifications to an internal web tool, improving system stability and operational efficiency.
               </li>
               <li>
-                Developed a robust, client-facing data analytics report,
-                empowering clients with data-driven insights to optimize HR
-                performance.
+                Developed a robust, client-facing data analytics report, empowering clients with data-driven insights to optimize HR performance.
               </li>
             </ul>
           </div>
@@ -80,12 +69,10 @@ const WorkHistory = () => {
             <p>02/2019 - 07/2019</p>
             <ul>
               <li>
-                Led successful event management, significantly enhancing brand
-                awareness.
+                Led successful event management, significantly enhancing brand awareness.
               </li>
               <li>
-                Contributed to business development, fostering increased
-                business opportunities.
+                Contributed to business development, fostering increased business opportunities.
               </li>
               <li>Enhanced social media engagement and brand awareness.</li>
               <li>
@@ -104,23 +91,17 @@ const WorkHistory = () => {
             <p>01/2015 - 04/2019</p>
             <ul>
               <li>
-                Initiated and cultivated a thriving Singapore-based startup
-                community on Google+ from my home, starting it from the ground
-                up.
+                Initiated and cultivated a thriving Singapore-based startup community on Google+ from my home, starting it from the ground up.
               </li>
               <li>
-                Successfully established a widely recognized community platform,
-                attracting over 7,500 users from across Asia, Singapore, and
-                globally.
+                Successfully established a widely recognized community platform, attracting over 7,500 users from across Asia, Singapore, and globally.
               </li>
               <li>
-                Developed a valuable community resource for learning about local
-                businesses and startups within the Singaporean ecosystem.
+                Developed a valuable community resource for learning about local businesses and startups within the Singaporean ecosystem.
               </li>
             </ul>
             <p>
-              Built an independent community platform on Google+ specifically
-              designed to support the local startup ecosystem.
+              Built an independent community platform on Google+ specifically designed to support the local startup ecosystem.
             </p>
           </div>
           <div className="line-break"></div>
@@ -129,19 +110,13 @@ const WorkHistory = () => {
             <p>07/2018 - 01/2019</p>
             <ul>
               <li>
-                Developed and distributed customer-centric hardware driver
-                packages and documentation, ensuring customers received
-                essential software for their purchased computers.
+                Developed and distributed customer-centric hardware driver packages and documentation, ensuring customers received essential software for their purchased computers.
               </li>
               <li>
-                Performed rigorous test data analysis and in-house testing
-                across a range of storage devices, including Solid-State Drives
-                (M.2, PCIe NVMe, SATA) and Hard Disk Drives (SATA3), to ensure
-                product reliability.
+                Performed rigorous test data analysis and in-house testing across a range of storage devices, including Solid-State Drives (M.2, PCIe NVMe, SATA) and Hard Disk Drives (SATA3), to ensure product reliability.
               </li>
               <li>
-                Established and nurtured strong vendor relationships,
-                facilitating effective communication and collaboration.
+                Established and nurtured strong vendor relationships, facilitating effective communication and collaboration.
               </li>
             </ul>
           </div>
@@ -153,32 +128,42 @@ const WorkHistory = () => {
             <p>10/2017 - 02/2018</p>
             <ul>
               <li>
-                Developed comprehensive documentation for the UOB Mighty
-                Application, ensuring clear understanding and user guidance.
+                Developed comprehensive documentation for the UOB Mighty Application, ensuring clear understanding and user guidance.
               </li>
               <li>
-                Engineered real-time price indicator features utilizing the
-                React Native JavaScript framework, enhancing application
-                functionality.
+                Engineered real-time price indicator features utilizing the React Native JavaScript framework, enhancing application functionality.
               </li>
             </ul>
           </div>
           <div className="line-break"></div>
           <div className="card--work-history">
-            <strong>🚧 Coding Instructor | Code Gakko</strong>
+            <strong>🚧 Workshop Facilitator | Code Gakko</strong>
             <p>07/2017 - 02/2018</p>
             <ul>
               <li>
-                Engaged students through hands-on lessons, fostering interactive
-                learning and practical application.
+                Engaged students through hands-on lessons, fostering interactive learning and practical application.
               </li>
               <li>
-                Developed students' programming intuition and coding
-                capabilities, empowering them to solve complex problems.
+                Developed students' programming intuition and coding capabilities, empowering them to solve complex problems.
               </li>
               <li>
-                Cultivated computational and algorithmic thinking through
-                real-world examples, bridging theory and practice.
+                Cultivated computational and algorithmic thinking through real-world examples, bridging theory and practice.
+              </li>
+            </ul>
+          </div>
+          <div className="line-break"></div>
+          <div className="card--work-history">
+            <strong>🚧 Workshop Facilitator | Witty Mitty</strong>
+            <p>07/2017 - 02/2018</p>
+            <ul>
+              <li>
+                Facilitated project-based learning to build practical coding skills and problem-solving abilities.
+              </li>
+              <li>
+                Mentored students in developing algorithmic logic and computational thinking through hands-on coding exercises.
+              </li>
+              <li>
+                Translated complex programming theories into practical application using interactive lessons and real-world examples.
               </li>
             </ul>
           </div>
@@ -188,18 +173,13 @@ const WorkHistory = () => {
             <p>01/2017 - 07/2017</p>
             <ul>
               <li>
-                Directed a multidisciplinary team of artists, developers, and
-                coordinators, ensuring alignment and achievement of project
-                milestones.
+                Directed a multidisciplinary team of artists, developers, and coordinators, ensuring alignment and achievement of project milestones.
               </li>
               <li>
-                Successfully led the rapid prototyping of a game on Unity 3D,
-                demonstrating agile development and technical expertise.
+                Successfully led the rapid prototyping of a game on Unity 3D, demonstrating agile development and technical expertise.
               </li>
               <li>
-                Facilitated effective cross-cultural communication to resolve
-                linguistic differences, enabling the successful completion of
-                the Game Design Document.
+                Facilitated effective cross-cultural communication to resolve linguistic differences, enabling the successful completion of the Game Design Document.
               </li>
             </ul>
           </div>
