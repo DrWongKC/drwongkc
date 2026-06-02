4619d277571a4d3317977b8dddf4f3da196f9154
Wong Kai Chong 黄恺聪 <KAI_Wong@outlook.com>
2026-06-02 17:00:40 +0800

    update digital resume's summary
    added my studying of AI powered software engineering into the summary list

--- src/components/Summary.tsx
+++ src/components/Summary.tsx
@@ -1,33 +1,39 @@
-import PhotoGrid from "./PhotoGrid"
+import PhotoGrid from "./PhotoGrid";
 
 const Summary = () => {
   return (
     <section className="summary--page">
-          <h2>Summary</h2>
-          <ul id="summary--list">
-            <li>
-              ✔️ 5+ Years experience in software engineering from Accenture,
-              GrabJobs and UOB Bank.
-            </li>
-            <li>
-              ✔️ Global Technology Challenge Event Co-organiser & Photographer,
-              Certified Professional Scrum Master™ I (PSM I) & former Co-Founder
-              of KAI Studio and Startup Singapore Google+.
-            </li>
-            <li>
-              ✔️ ex-Tech educator in Springdale Primary School, UWCSEA East
-              Campus & Dulwich College.
-            </li>
-            <li>
-              ✔️ Effective communicator and empathetic team member, contributing to problem-solving efforts.
-            </li>
-            <li>
-              ✔️ I am proficient in writing, managing projects, and delivering effective presentations.
-            </li>
-          </ul>
-          <PhotoGrid />
-        </section>
-  )
-}
+      <h2>Summary</h2>
+      <ul id="summary--list">
+        <li>
+          ✔️ Completed a paid online learning resource titled Build AI-Powered
+          Apps From Code With Mosh
+        </li>
+        <li>
+          ✔️ 5+ Years experience in software engineering from Accenture,
+          GrabJobs and UOB Bank.
+        </li>
+        <li>
+          ✔️ Global Technology Challenge Event Co-organiser & Photographer,
+          Certified Professional Scrum Master™ I (PSM I) & former Co-Founder of
+          KAI Studio and Startup Singapore Google+.
+        </li>
+        <li>
+          ✔️ ex-Tech educator in Springdale Primary School, UWCSEA East Campus &
+          Dulwich College.
+        </li>
+        <li>
+          ✔️ Effective communicator and empathetic team member, contributing to
+          problem-solving efforts.
+        </li>
+        <li>
+          ✔️ I am proficient in writing, managing projects, and delivering
+          effective presentations.
+        </li>
+      </ul>
+      <PhotoGrid />
+    </section>
+  );
+};
 
-export default Summary
+export default Summary;
