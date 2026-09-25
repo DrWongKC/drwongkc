dffb583cbd5f6cd44537ad1cc3187f9521367426
Wong Kai Chong 黄恺聪 <KAI_Wong@outlook.com>
2026-09-25 12:56:21 +0800

    updated application to use Raleway as default font for the website

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -14,9 +14,9 @@ function App() {
       <div id="container--main">
         {/* Navigation */}
         <nav>
-          <Link to="/drwongkc">Home</Link> ⧸ <Link to="/about">About</Link> ⧸{" "}
-          <Link to="/intellifindlaptopguide">IntelliFind</Link>
-          {/* ⧸{" "}<Link to="/statement">AI SoC</Link> */}
+          <Link to="/drwongkc">Home</Link> ⧸ <Link to="/about">About</Link>
+          {/* ⧸{" "}<Link to="/intellifindlaptopguide">IntelliFind</Link> */}
+          {/* ⧸ <Link to="/statement">AI SoC</Link> */}
         </nav>
         {/* Routes */}
         <Routes>
